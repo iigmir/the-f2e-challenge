1e0244b02c89afc98998859a156ea4163dfbb942
iigmir <roc120j@gmail.com>
2018-06-03 00:26:10 +0800

    Add index componet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { NavbarComponent } from "./navbar/navbar.component";
 import { AppRoutingModule } from './/app-routing.module';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        NavbarComponent
+        NavbarComponent,
+        IndexComponent
     ],
     imports: [
         BrowserModule,
