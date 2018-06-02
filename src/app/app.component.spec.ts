67acad007adf103679d8136667f4f328395e9796
iigmir <roc120j@gmail.com>
2018-06-02 22:09:06 +0800

    Fix TSLint

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
-describe('AppComponent', () =>
+import { TestBed, async } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
+
+describe("AppComponent", () =>
 {
     beforeEach(async(() =>
     {
@@ -10,8 +11,8 @@ describe('AppComponent', () =>
             ],
         }).compileComponents();
     }));
-    
-    it('should create the app', async(() =>
+
+    it("should create the app", async(() =>
     {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.debugElement.componentInstance;
@@ -21,13 +22,13 @@ describe('AppComponent', () =>
     {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.debugElement.componentInstance;
-        expect(app.title).toEqual('前端修練精神時光屋');
+        expect(app.title).toEqual("前端修練精神時光屋");
     }));
-    it('should render title in a h1 tag', async(() =>
+    it("should render title in a h1 tag", async(() =>
     {
         const fixture = TestBed.createComponent(AppComponent);
         fixture.detectChanges();
         const compiled = fixture.debugElement.nativeElement;
-        expect(compiled.querySelector('h1').textContent).toContain('前端修練精神時光屋');
+        expect(compiled.querySelector("h1").textContent).toContain("前端修練精神時光屋");
     }));
 });
