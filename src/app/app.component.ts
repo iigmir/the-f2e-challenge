2136a7dbf2b97d4664dc12148beeaae03bd8e5b1
iigmir <roc120j@gmail.com>
2018-06-02 22:47:07 +0800

    install virefox plugin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.css"]
 })
+
 export class AppComponent {
-  title = '前端修練精神時光屋';
+    title = "前端修練精神時光屋";
 }
