b66aa7c183236b2996662507f5305ad5b4b571a8
iigmir <roc120j@gmail.com>
2018-06-02 21:11:20 +0800

    TDD app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,6 +22,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to the-f2e-challenge!');
+    expect(compiled.querySelector('h1').textContent).toContain('歡迎來到前端精神時光屋的作品集！');
   }));
 });
