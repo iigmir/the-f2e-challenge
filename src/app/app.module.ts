d324dffd86b169879f0d503270c806076f1ac528
iigmir <roc120j@gmail.com>
2018-06-03 00:16:46 +0800

    Add router and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        NavbarComponent
     ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        AppRoutingModule
     ],
     providers: [],
     bootstrap: [AppComponent]
 })
+
 export class AppModule { }
